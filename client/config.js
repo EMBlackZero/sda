fe5ecdcb1381e1ac062e1d3823be906e1e81c8ce
BlackZero <kingch202@gmailcom>
2025-03-03 23:25:07 +0700

    limit database

--- client/config.js
+++ client/config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
   isProd,
-  serverUrlPrefix: isProd ? 'https://sda-556323513019.asia-southeast1.run.app' : 'https://sda-556323513019.asia-southeast1.run.app'
+  serverUrlPrefix: isProd ? 'https://sda-556323513019.asia-southeast1.run.app' : 'http://localhost:1337',
 }
 
 export default config;
--- server/config/api.ts
+++ server/config/api.ts
@@ -1,7 +1,7 @@
 export default {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
-    withCount: true,
+    defaultLimit: 1000, // ค่าเริ่มต้นสูง (เช่น 1000) หรือไม่ต้องกำหนดก็ได้
+    maxLimit: null,     // ตั้งเป็น null เพื่อปลดลิมิตสูงสุด
+    withCount: true,    // คงค่าเดิมไว้ หากต้องการนับจำนวนข้อมูล
   },
 };
