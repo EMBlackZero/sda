ace48cf25a46278a2a4e9e4fc8abc8c881ecf61a
BlackZero <kingch202@gmailcom>
2025-03-05 20:10:52 +0700

    add logo to header and update favicon

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -202,7 +202,15 @@ export default function Home() {
 
         {/* Header */}
         <div className="p-7 flex justify-between items-center sm:p-7 shrink-0">
-          <h1 className="text-xl font-bold text-white">Secret Room Chat</h1>
+          <div className="flex items-center">
+            <img
+              src="/logo3.png"
+              alt="Logo"
+              className="h-8 w-8 mr-2"
+            />
+            <h1 className="text-xl font-bold text-white">Secret Room Chat</h1>
+
+          </div>
           <div className="text-white flex justify-between items-center">
             <span className="hidden sm:inline truncate max-w-[200px] sm:max-w-none">{username}</span>
             <button
